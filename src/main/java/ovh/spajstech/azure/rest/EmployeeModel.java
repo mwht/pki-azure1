6920569633b2c727a2bdbdf65a737d7492187768
Lukkario <michallukianski.0@gmail.com>
2019-04-10 17:26:02 +0200

    Added EmployeeModel

---
+++ src/main/java/ovh/spajstech/azure/rest/EmployeeModel.java
@@ -0,0 +1,42 @@
+package ovh.spajstech.azure.rest;
+
+public class EmployeeModel {
+    private String firstName;
+    private String lastName;
+    private Integer id;
+
+    public EmployeeModel(String firstName, String lastName, Integer id) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.id = id;
+    }
+
+    public EmployeeModel()
+    {
+
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
