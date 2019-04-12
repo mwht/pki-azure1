5f9fc84dadd6805321897095b650f2623a6cafb4
Sebastian Madejski <209388@edu.p.lodz.pl>
2019-04-12 08:20:01 +0200

    add IndexController

---
+++ src/main/java/ovh/spajstech/azure/rest/IndexController.java
@@ -0,0 +1,21 @@
+package ovh.spajstech.azure.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import ovh.spajstech.azure.model.IndexView;
+import ovh.spajstech.azure.repository.EmployeeRepository;
+import ovh.spajstech.azure.repository.PlacesRepository;
+
+@RestController
+public class IndexController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    private PlacesRepository placesRepository;
+
+    public IndexView index() {
+        return new IndexView(employeeRepository.findAll(), placesRepository.findAll());
+    }
+}
