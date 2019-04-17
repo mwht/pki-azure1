c27ad23b87f98388b14213ea30bfab525e92bf37
Sebastian Madejski <madejski.s97@gmail.com>
2019-04-17 07:46:55 +0200

    add support for Tomcat

---
+++ src/main/java/ovh/spajstech/azure/SpringBootTomcatApplication.java
@@ -0,0 +1,6 @@
+package ovh.spajstech.azure;
+
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class SpringBootTomcatApplication extends SpringBootServletInitializer {
+}
